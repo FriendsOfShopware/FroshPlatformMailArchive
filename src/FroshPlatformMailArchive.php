139aab65198b91246ccda04919764382f5a4c71c
Soner Sayakci <s.sayakci@shopware.com>
2021-09-09 19:31:55 +0000

    fix: Set cleanup on installation to 90 days

--- src/FroshPlatformMailArchive.php
+++ src/FroshPlatformMailArchive.php
@@ -4,10 +4,17 @@ namespace Frosh\MailArchive;
 
 use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class FroshPlatformMailArchive extends Plugin
 {
+    public function install(InstallContext $installContext): void
+    {
+        $this->container->get(SystemConfigService::class)->set('FroshPlatformMailArchive.config.deleteMessageAfterDays', 90);
+    }
+
     public function uninstall(UninstallContext $uninstallContext): void
     {
         if ($uninstallContext->keepUserData()) {
