90e980ba9ea264ea0f0d2b8d7fcb556df202be50
Erik Pöhler <info@teuton.mx>
2022-08-18 12:16:23 +0200

    fix uninstall

--- src/FroshPlatformMailArchive.php
+++ src/FroshPlatformMailArchive.php
@@ -1,5 +1,6 @@
-<?php declare(strict_types=1);
+<?php
 
+declare(strict_types = 1);
 namespace Frosh\MailArchive;
 
 use Doctrine\DBAL\Connection;
@@ -8,13 +9,13 @@ use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class FroshPlatformMailArchive extends Plugin
 {
+
     public function uninstall(UninstallContext $uninstallContext): void
     {
         if ($uninstallContext->keepUserData()) {
             return;
         }
 
-        $this->container->get(Connection::class)->executeQuery('DROP TABLE IF EXISTS frosh_mail_archive');
+        $this->container->get(Connection::class)->executeStatement('DROP TABLE IF EXISTS frosh_mail_archive');
     }
-
 }
