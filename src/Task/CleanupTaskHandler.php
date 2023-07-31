c351dfed17cfc8abd8005ef1aa44416c6fe1ae72
tinect <s.koenig@tinect.de>
2023-07-31 23:56:37 +0200

    feat: cleanup eml files when task is running

--- src/Task/CleanupTaskHandler.php
+++ src/Task/CleanupTaskHandler.php
@@ -4,9 +4,11 @@ namespace Frosh\MailArchive\Task;
 
 use Doctrine\DBAL\Connection;
 use Frosh\MailArchive\Content\MailArchive\MailArchiveDefinition;
+use League\Flysystem\FilesystemOperator;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
+use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
@@ -16,7 +18,8 @@ class CleanupTaskHandler extends ScheduledTaskHandler
     public function __construct(
         EntityRepository $scheduledTaskRepository,
         private readonly SystemConfigService $configService,
-        private readonly Connection $connection
+        private readonly Connection $connection,
+        private readonly FilesystemOperator $privateFilesystem
     ) {
         parent::__construct($scheduledTaskRepository);
     }
@@ -33,7 +36,9 @@ class CleanupTaskHandler extends ScheduledTaskHandler
         $time->modify(sprintf('-%s days', $days));
 
         $query = $this->connection->createQueryBuilder();
-        $query->delete(MailArchiveDefinition::ENTITY_NAME);
+
+        $query->select('id', 'eml_path');
+        $query->from(MailArchiveDefinition::ENTITY_NAME);
         $query->where(
             $query->expr()->lte(
                 'created_at',
@@ -41,6 +46,27 @@ class CleanupTaskHandler extends ScheduledTaskHandler
             )
         );
 
-        $query->executeQuery();
+        $result = $query->executeQuery()->fetchAllAssociative();
+
+        if (count($result) === 0) {
+            return;
+        }
+
+        foreach ($result as $item) {
+            if ($this->privateFilesystem->has($item['eml_path'])) {
+                $this->privateFilesystem->delete($item['eml_path']);
+            }
+        }
+
+        $deleteQuery = $this->connection->createQueryBuilder();
+        $deleteQuery->delete(MailArchiveDefinition::ENTITY_NAME);
+        $deleteQuery->where(
+            $query->expr()->in(
+                'LOWER(HEX(id))',
+                \array_map(fn($item) => '"' . Uuid::fromBytesToHex($item['id']) . '"', $result)
+            )
+        );
+
+        $deleteQuery->executeQuery();
     }
 }
