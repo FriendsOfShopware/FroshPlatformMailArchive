73a858478795abbcd1ed5cab69d3475461d2d914
tinect <s.koenig@tinect.de>
2023-09-14 15:56:44 +0200

    feat: test phunit

--- src/Services/MailSender.php
+++ src/Services/MailSender.php
@@ -103,7 +103,6 @@ class MailSender extends AbstractMailSender
 
     /**
      * @param Address[] $addresses
-     * @return array<string, string>
      */
     private function convertAddress(array $addresses): array
     {
