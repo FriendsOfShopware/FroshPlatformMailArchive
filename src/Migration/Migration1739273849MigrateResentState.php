aa293994e5c37fb8615a0ff2e85461b51e27d930
tinect <s.koenig@tinect.de>
2025-02-11 12:57:17 +0100

    feat: migrate existing resent mails to new state

---
+++ src/Migration/Migration1739273849MigrateResentState.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\MailArchive\Migration;
+
+use Doctrine\DBAL\ArrayParameterType;
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1739273849MigrateResentState extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1739273849;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sourceMailIds = $connection->fetchFirstColumn('SELECT source_mail_id FROM frosh_mail_archive;');
+
+        if (empty($sourceMailIds)) {
+            return;
+        }
+
+        $updateQuery = $connection->createQueryBuilder();
+        $updateQuery->update('frosh_mail_archive');
+        $updateQuery->set('transport_state', '\'resent\'');
+        $updateQuery->where('id IN (:ids)');
+
+        foreach (array_chunk($sourceMailIds, 1000) as $chunk) {
+            $updateQuery->setParameter('ids', $chunk, ArrayParameterType::STRING);
+            $updateQuery->executeStatement();
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void {}
+}
