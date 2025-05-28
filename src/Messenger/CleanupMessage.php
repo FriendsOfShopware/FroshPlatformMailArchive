9610c55159092b556459be305f76fdca5e8bed76
Wanne Van Camp <wanne.vancamp@meteor.be>
2025-05-28 08:40:31 +0200

    create cleanup message handler

---
+++ src/Messenger/CleanupMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frosh\MailArchive\Messenger;
+
+use Shopware\Core\Framework\MessageQueue\AsyncMessageInterface;
+
+class CleanupMessage implements AsyncMessageInterface
+{
+
+}
